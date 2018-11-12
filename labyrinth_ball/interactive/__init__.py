383bc5a3be320f0d191f0537815cd59a5a54ad2c
James Dowdell <james@ptowngames.com>
2018-11-11 19:06:00 -0800

    odds n ends

---
+++ labyrinth_ball/interactive/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+#
+# STUB
+#
+# TODO - technically interactive/ shouldn't have library code.  Refactor
+# makeModelFromSineMap so the stuff that's being used elsewhere is in
+# common/ instead.
